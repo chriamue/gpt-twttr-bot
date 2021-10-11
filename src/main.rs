c7173a5b3214dc0ef70efc87c61373e754bc440f
chriamue <chriamue@gmail.com>
2021-10-11 15:07:21 +0100

    :card_file_box: store already processed tweet ids into file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,32 @@ use egg_mode::error::Result;
 use gptj;
 use std::env;
 
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn read_db(filename: impl AsRef<Path>) -> Vec<u64> {
+    match File::open(filename) {
+        Ok(file) => {
+            let buf = BufReader::new(file);
+            buf.lines()
+                .map(|l| l.expect("Could not parse line").parse::<u64>().unwrap())
+                .collect()
+        }
+        Err(_) => vec![],
+    }
+}
+
+fn write_db(tweets: Vec<u64>, filename: impl AsRef<Path>) {
+    let mut f = File::create(filename).expect("Unable to create file");
+    for i in &tweets {
+        let id = format!("{}\n", i.to_string());
+        f.write_all(id.as_bytes()).expect("Unable to write data");
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let con_key = env::var("KEY").unwrap();
@@ -17,14 +43,21 @@ async fn main() -> Result<()> {
         access: access_token,
     };
 
+    let mut tweets: Vec<u64> = read_db("tweets.db");
+
     let timeline = egg_mode::tweet::home_timeline(&token).with_page_size(10);
 
     let (_timeline, feed) = timeline.start().await?;
     for tweet in feed.response {
-        println!("");
-        println!("{}: {}", &tweet.user.unwrap().screen_name, &tweet.text);
-        println!("ai: {}", ai_tweet(tweet.text.to_string()).await);
+        if !tweets.contains(&tweet.id) {
+            println!("");
+            println!("{}: {}", &tweet.user.unwrap().screen_name, &tweet.text);
+            println!("ai: {}", ai_tweet(tweet.text.to_string()).await);
+            println!("---");
+            tweets.push(tweet.id);
+        }
     }
+    write_db(tweets, "tweets.db");
     Ok(())
 }
 
