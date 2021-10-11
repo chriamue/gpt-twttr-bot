1e49e092d33024a6c497c9a5f3b8f4099776f29e
chriamue <chriamue@gmail.com>
2021-10-11 13:59:33 +0100

    :sparkles: generate answer for tweet using gpt-j

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use egg_mode::error::Result;
+use gptj;
 use std::env;
 
 #[tokio::main]
@@ -10,22 +11,25 @@ async fn main() -> Result<()> {
 
     let con_token = egg_mode::KeyPair::new(con_key, con_secret);
 
-    let access_token = egg_mode::KeyPair::new(
-        access_token,
-        access_token_secret
-    );
+    let access_token = egg_mode::KeyPair::new(access_token, access_token_secret);
     let token = egg_mode::Token::Access {
         consumer: con_token,
         access: access_token,
     };
 
-    let timeline =
-        egg_mode::tweet::home_timeline(&token).with_page_size(5);
+    let timeline = egg_mode::tweet::home_timeline(&token).with_page_size(10);
 
     let (_timeline, feed) = timeline.start().await?;
     for tweet in feed.response {
         println!("");
         println!("{}: {}", &tweet.user.unwrap().screen_name, &tweet.text);
+        println!("ai: {}", ai_tweet(tweet.text.to_string()).await);
     }
     Ok(())
+}
+
+async fn ai_tweet(context: String) -> String {
+    let gpt = gptj::GPT::default();
+    let response = gpt.generate(context, 42, 0.9, 0.9, None).await.unwrap();
+    response.text
 }
